97275149113c87bc7dc73a295ffce44cc866aad2
sunc <[728140347@qq.com]>
2018-03-13 11:23:41 +0800

    违章详情修改

--- 车辆管家/pages/wzxq/wzxq.js
+++ 车辆管家/pages/wzxq/wzxq.js
@@ -6,38 +6,37 @@ Page({
   data: {
     detailInfos:[],
     hphm:'',
-    carList: '',
     wzxx:[],
     loading: true,
     loadingsuccess: false
   },
 
   onLoad: function (options) {
-    // this.getList()
+    this.getList()
     this.setData({
-      hphm: options.hphm,
-      carList: options.carList
+      hphm: options.hphm
     })
     console.log(options.carList)
+    console.log(typeof options.carList)
   },
-  // getList: function () {
-  //   var that = this
-  //   wx.request({
-  //     url: app.globalData.baseurl + "/weixin/clgj/wdcl?userGuid=" + app.globalData.userGuid,
-  //     success: function (res) {
-  //       var detailInfos = res.data.data.resultCar.mycarList;
-  //       for (var i = 0; i < detailInfos.length;i++){
-  //         if (that.data.hphm === detailInfos[i].hphm){
-  //           that.setData({
-  //             detailInfos:detailInfos[i],
-  //             loading: false,
-  //             loadingsuccess: true
-  //           })
-  //         }
-  //       }
-  //     }
-  //   })
-  // },
+  getList: function () {
+    var that = this
+    wx.request({
+      url: app.globalData.baseurl + "/weixin/clgj/wdcl?userGuid=" + app.globalData.userGuid,
+      success: function (res) {
+        var detailInfos = res.data.data.resultCar.mycarList;
+        for (var i = 0; i < detailInfos.length;i++){
+          if (that.data.hphm === detailInfos[i].hphm){
+            that.setData({
+              detailInfos:detailInfos[i],
+              loading: false,
+              loadingsuccess: true
+            })
+          }
+        }
+      }
+    })
+  },
   getUserInfo: function(e) {
     console.log(e)
     app.globalData.userInfo = e.detail.userInfo
