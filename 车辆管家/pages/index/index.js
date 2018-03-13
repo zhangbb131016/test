d935217f287a963956908d0028c6566a170b1782
zhangbb <zhangbb@oceasoft>
2018-03-13 14:22:23 +0800

    样式问题修改

--- 车辆管家/pages/index/index.js
+++ 车辆管家/pages/index/index.js
@@ -5,9 +5,62 @@ const app = getApp()
 Page({
   data: {
     carList:[],
-    carListString: '',
-    loading:true
+    showModalStatus: false,
+      loading:true
   },
+
+  powerDrawer: function (e) {
+    var currentStatu = e.currentTarget.dataset.statu;
+    this.util(currentStatu)
+  },
+  util: function (currentStatu) {
+    /* 动画部分 */
+    // 第1步：创建动画实例
+    var animation = wx.createAnimation({
+      duration: 200,  //动画时长
+      timingFunction: "linear", //线性
+      delay: 0  //0则不延迟
+    });
+
+    // 第2步：这个动画实例赋给当前的动画实例
+    this.animation = animation;
+
+    // 第3步：执行第一组动画
+    animation.opacity(0).rotateX(-100).step();
+
+    // 第4步：导出动画对象赋给数据对象储存
+    this.setData({
+      animationData: animation.export()
+    })
+
+    // 第5步：设置定时器到指定时候后，执行第二组动画
+    setTimeout(function () {
+      // 执行第二组动画
+      animation.opacity(1).rotateX(0).step();
+      // 给数据对象储存的第一组动画，更替为执行完第二组动画的动画对象
+      this.setData({
+        animationData: animation
+      })
+
+      //关闭
+      if (currentStatu == "close") {
+        this.setData(
+          {
+            showModalStatus: false
+          }
+        );
+      }
+    }.bind(this), 200)
+
+    // 显示
+    if (currentStatu == "open") {
+      this.setData(
+        {
+          showModalStatus: true
+        }
+      );
+    }
+  } ,
   //添加车辆
   addCar:function()
   {
@@ -33,10 +86,8 @@ Page({
         var carInfos = res.data.data.resultCar.mycarList;
         that.setData({
           carList: carInfos,
-          loading: false,
-          carListString: JSON.stringify(that.data.carList)
+          loading: false
         })
-        console.log(JSON.stringify(that.data.carList))
       }
     })
   },
@@ -66,11 +117,11 @@ Page({
             }
           })
         } else if (res.cancel) {
-          
+
         }
       }
     })
-    
+
   },
   getUserInfo: function(e) {
     console.log(e)
