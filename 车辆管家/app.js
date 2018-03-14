417902079584c270e11bf5ba82411f86e748c25b
李威 <liwei58858@163.com>
2018-03-14 10:42:43 +0800

    登录实现。

--- 车辆管家/app.js
+++ 车辆管家/app.js
@@ -1,14 +1,39 @@
 //app.js
 App({
   onLaunch: function () {
+    var that = this
+    wx.login({
+      success:(function(res){
+        that.globalData.code=res.code
+      })
+    })
 
+    //同步读取用户数据
+    var userInfo = wx.getStorageSync("userInfo")
+    if(userInfo)
+    {
+      that.globalData.isLogin = userInfo.isLogin
+      that.globalData.userGuid = userInfo.userInfo.guid
+      that.globalData.userInfo = userInfo.data
+    }
   },
-  data:({
-    isLogin:false,
-    mainUrl:"https://gaapi.jl.gov.cn/econsole/"
-  }),
   globalData: {
+
+    //登录状态（经过考虑，不使用自动登录，把状态保存在本地，用于进入应用的一个判定）
+    isLogin: false,
+
+    //登录状态改变（用于登录后刷新各个页面）
+    loginStatuChange:false,
+
+    //基础接口请求地址
     baseurl: 'https://gaapi.jl.gov.cn/econsole',
-    userGuid: '4377d0a6-58fd-498b-8cab-7b74f069939c'
+
+    userGuid: '4377d0a6-58fd-498b-8cab-7b74f069939c',
+
+    //用户授权code
+    code:"",
+
+    //用户数据（guid）等
+    userInfo:{}
   }
 })
--- 车辆管家/pages/login/login.js
+++ 车辆管家/pages/login/login.js
@@ -22,7 +22,29 @@ function countdown(that) {
   }
     , 1000)
 }
-
+function getImageCode(that)
+{
+  //加载图形验证码
+  var that = that
+  var random = "";
+  for (var i = 0; i < 6; i++) {
+    random = random + Math.floor(Math.random() * 10)
+  }
+  console.log(random)
+  that.setData({
+    randRomNum: random
+  })
+  wx.request({
+    url: getApp().globalData.baseurl + "/api/sms/createImgCode?id=" + that.data.randRomNum,
+    success: (function (res) {
+      var str = res.data.data.replace(/\s+/g, "")
+      that.setData({
+        imageCodeUrl: 'data:image/png;base64,' + str
+      })
+      console.log(that.data.imageCodeUrl)
+    })
+  })
+}
 
 Page({
 
@@ -32,21 +54,103 @@ Page({
   data: {
     isPhoneLength:false,
     timeOutString:"获取验证码",
-    second:60
+    second:60,
+    randRomNum:"",
+    imageCodeUrl:"",
+    phoneNum:"",
+    imageCodeStr:"",
+    codeString:""
   },
 
   //登录
   login: function () {
-    wx.navigateBack({
-      delta:2
-    })
+    console.log(this.data.phoneNum)
+    if (this.data.phoneNum.length == 0)
+    {
+      wx.showToast({
+        title: "请输入手机号",
+        image: "/images/toast_error.png"
+      })
+    }
+    else if (this.data.phoneNum.length != 11){
+      wx.showToast({
+        title: "请输入正确的手机号",
+        image: "/images/toast_error.png"
+      })
+    }
+    else{
+      if (this.data.codeString.length == 0)
+      {
+        wx.showToast({
+          title: "请输入验证码",
+          image: "/images/toast_error.png"
+        })
+      } else if (this.data.codeString.length != 6)
+      {
+        wx.showToast({
+          title: "请输入正确的验证码",
+          image: "/images/toast_error.png"
+        })
+      }else{
+        console.log(getApp().globalData.code)
+        wx.request({
+          url: getApp().globalData.baseurl +"/weixin/clgj/auth",
+
+          header: {
+            'content-type': 'application/x-www-form-urlencoded'
+          },
+          data:{
+            mobile:this.data.phoneNum,
+            validateCode: this.data.codeString,
+            code: getApp().globalData.code
+          },
+          method:"POST",
+          success: (function(res){
+            if(res.data.succ)
+            {
+              var userInfo = {"isLogin":true,"userInfo":res.data.data}
+              var info = {}
+              wx.setStorage({
+                key: 'userInfo',
+                data: userInfo
+              })
+
+              getApp().globalData.isLogin = true
+              getApp().globalData.userInfo = res.data.data
+              getApp().globalData.userGuid = res.data.data.guid
+              getApp().globalData.loginStatuChange = true
+
+              wx.showToast({
+                title: res.data.msg
+              })
+
+              wx.navigateBack({
+                delta:2
+              })
+
+            }else{
+              wx.showToast({
+                title: res.data.msg,
+                image: "/images/toast_error.png"
+              })
+            }
+          })
+        })
+      }
+    }
   },
 
   //手机号输入框监听
   phoneImport: function(res)
   {
-    var length1 = res.detail.value.length
-    if (length1 == 11)
+    var phonelength = res.detail.value.length
+    var imageCodelength = this.data.imageCodeStr.length
+    console.log(res.detail.value)
+    this.setData({
+      phoneNum: res.detail.value
+    })
+    console.log(phonelength, imageCodelength)
+    if (phonelength == 11 && imageCodelength ==4)
     {
       this.setData({
         isPhoneLength:true
@@ -57,26 +161,85 @@ Page({
       })
     }
   },
+  //图形验证码输入
+  imageCodeImport:function(res)
+  {
+    var phonelength = this.data.phoneNum.length
+    var imageCodelength = res.detail.value.length
+    this.setData({
+      imageCodeStr: res.detail.value
+    })
+    console.log(phonelength,imageCodelength)
+    if (phonelength == 11 && imageCodelength == 4) {
+      this.setData({
+        isPhoneLength: true
+      })
+    } else {
+      this.setData({
+        isPhoneLength: false
+      })
+    }
+  },
+  //短信验证码监听
+  codeImport:function(res)
+  {
+    console.log(res.detail.value)
+    this.setData({
+      codeString: res.detail.value
+    })
+  },
+
   //获取验证码
   getCode:function()
   {
+    var that = this
     if (this.data.isPhoneLength)
     {
-       countdown(this)
-    }else
-    {
+      wx.request({
+        url: getApp().globalData.baseurl +"/api/sms/send",
+        header: {
+          'content-type': 'application/x-www-form-urlencoded'
+        },
+        data: {
+          tel: this.data.phoneNum,
+          type: "2",
+          expire:"",
+          content:"",
+          id: this.data.randRomNum,
+          imgCode: this.data.imageCodeStr,
+          pt :"I"
+        },
+        method:"POST",
+        success: function (res) 
+        {
+          if(res.data.succ)
+          {
+            countdown(that)
+            wx.showToast({
+              title: res.data.msg,
+            })
+          }else{
+            getImageCode(that)
+            wx.showToast({
+              title: res.data.msg,
+              image:"/images/toast_error.png"
+            })
+          }
+          console.log(res)
+        }
+      })
 
     }
 },
+
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    wx.showModal({
-      title: '温馨提示',
-      content: '1、本平台仅支持小型车辆使用车主驾驶证处理非现场交通违法记录的情形。',
-    })
+
+    getImageCode(this)
+
   },
 
   /**
